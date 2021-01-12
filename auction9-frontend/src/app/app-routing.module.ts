e27263d15b785ed33e69f2ed319fa80ab646accd
dragana <d.zabaljac@gmail.com>
2021-01-12 17:01:04 +0100

    routing set up

--- auction9-frontend/src/app/app-routing.module.ts
+++ auction9-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserAuctionsComponent } from './user-auctions/user-auctions.component';
+import { WonAuctionsComponent } from './won-auctions/won-auctions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'myAuctions',
+    component: UserAuctionsComponent
+  },
+  {
+    path: 'wonAuctions',
+    component: WonAuctionsComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ auction9-frontend/src/app/user-auctions/user-auctions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-auctions',
+  templateUrl: './user-auctions.component.html',
+  styleUrls: ['./user-auctions.component.scss']
+})
+export class UserAuctionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
