a4569c6105bc483379cda0f52edae95e3e408e25
dragana <d.zabaljac@gmail.com>
2021-01-14 09:20:50 +0100

    added tbl prefix to table names

--- auction9-backend/db_script.sql
+++ auction9-backend/db_script.sql
@@ -6,7 +6,7 @@ DROP DATABASE auction9;
 CREATE DATABASE auction9;
 use auction9;
 
-CREATE TABLE auction (
+CREATE TABLE tbl_auction (
   auctionID bigint NOT NULL AUTO_INCREMENT,
   title varchar(40) NOT NULL,
   description varchar(300) NOT NULL,
@@ -17,12 +17,12 @@ CREATE TABLE auction (
   created_by bigint NOT NULL,
   winner bigint,
   PRIMARY KEY (auctionID),
-  CONSTRAINT created_by FOREIGN KEY (created_by) REFERENCES user (userID),
-  CONSTRAINT winner FOREIGN KEY (winner) REFERENCES user (userID),
+  CONSTRAINT created_by FOREIGN KEY (created_by) REFERENCES tbl_user (userID),
+  CONSTRAINT winner FOREIGN KEY (winner) REFERENCES tbl_user (userID),
   CONSTRAINT chk_status CHECK (status in ('INACTIVE', 'ACTIVE', 'FINISHED', 'REALIZED')) 
 )ENGINE=INNODB;
 
-CREATE TABLE user (
+CREATE TABLE tbl_user (
   userID bigint NOT NULL AUTO_INCREMENT,
   name varchar(30) NOT NULL,
   surname varchar(30) NOT NULL,
@@ -31,15 +31,15 @@ CREATE TABLE user (
   UNIQUE KEY (email)
 )ENGINE=INNODB;
 
-CREATE TABLE user_auction (
+CREATE TABLE tbl_user_auction (
   user_auction_ID bigint NOT NULL AUTO_INCREMENT,
   userID bigint NOT NULL,
   auctionID bigint NOT NULL,
   price decimal(10,2) NOT NULL,
   time datetime NOT NULL,
   PRIMARY KEY (user_auction_ID),
-  CONSTRAINT userID FOREIGN KEY (userID) REFERENCES user (userID),
-  CONSTRAINT auctionID FOREIGN KEY (auctionID) REFERENCES auction (auctionID)
+  CONSTRAINT userID FOREIGN KEY (userID) REFERENCES tbl_user (userID),
+  CONSTRAINT auctionID FOREIGN KEY (auctionID) REFERENCES tbl_auction (auctionID)
 )ENGINE=INNODB;
 
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
